e1b84cef9716beaad47b142127da97f099c5561f
Kina Lee <kina.lee@holbertonschool.com>
2016-12-19 20:17:43 +0000

    new shell

--- simple_shell.c
+++ simple_shell.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "header.h"
 
-int main(void)
+int main(int ac, char **av)
 {
-	char *buffer;
-	size_t size = 100;
+	char *buffer, *argv[1024];
+	int i, status;
 	int c = 1;
+	size_t size = 1024;
+	pid_t pid;
+	(void)ac;
 
 	while (c != EOF)
 	{
@@ -18,16 +20,36 @@ int main(void)
 
 		printf("$ ");
 		c = getline(&buffer, &size, stdin);
-
 		if (c == EOF)
 		{
 			printf("\n");
 			free(buffer);
 			exit(0);
 		}
+		argv[0] = strtok(buffer, " \t\n");
+		for (i = 1; argv[i - 1] != NULL; ++i)
+		{
+			argv[i] = strtok(NULL, " \t\n");
+		}
+		--i;
 
-		printf("%s", buffer);
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("Errors");
+			exit(-1);
+		}
+		if (pid == 0)
+		{
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				perror(av[0]);
+			}
+			exit(0);
+		}
+
+		wait(&status);
 	}
-
+	free(buffer);
 	return (0);
 }
